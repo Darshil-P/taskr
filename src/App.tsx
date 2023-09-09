7bf5b53f824bb41bbdb3244566959cf2d4abfbf9
Darshil Pipalia <darshilpipalia10@gmail.com>
2023-09-10 00:55:03 +0530

    Display Tasks List

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import './App.css';
+import "./App.css";
+import TaskTable from "./components/tasktable";
 
 function App() {
+  const tasks = [
+    {
+      id: crypto.randomUUID(),
+      title: "task1",
+      description: "description1",
+      completed: false,
+      dueDate: new Date("2023-10-1"),
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "task2",
+      description: "description2",
+      completed: false,
+      dueDate: new Date("2023-10-10"),
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "task3",
+      description: "description3",
+      completed: true,
+      dueDate: new Date("2023-10-20"),
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "task4",
+      description: "description4",
+      completed: true,
+      dueDate: new Date("2023-10-30"),
+    },
+  ];
   return (
-    <h1><span className='badge bg-secondary'>Hello World</span></h1>
-  )
+    <>
+      <TaskTable
+        items={tasks}
+        onChange={(task) => console.log(`Change! ${task.title}`)}
+        onDelete={(task) => console.log(`Delete! ${task.title}`)}
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/tasktable.tsx
@@ -0,0 +1,81 @@
+import { FunctionComponent } from "react";
+
+type Task = {
+  id: `${string}-${string}-${string}-${string}-${string}`;
+  title: string;
+  description: string;
+  completed: boolean;
+  dueDate: Date;
+};
+
+interface TaskTableProps {
+  items: Array<Task>;
+  onChange: (task: Task) => void;
+  onDelete: (task: Task) => void;
+}
+
+const TaskTable: FunctionComponent<TaskTableProps> = ({
+  items,
+  onChange: handleStatusChange,
+  onDelete: handleDelete,
+}) => {
+  {
+    if (items.length == 0) return "No Tasks Found";
+  }
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Status</th>
+          <th scope="col">Title</th>
+          <th scope="col">Description</th>
+          <th scope="col">Due</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((task, index) => {
+          return (
+            <tr
+              key={task.id}
+              className={task.completed ? "opacity-50 line-through" : ""}
+            >
+              <th scope="row">{index + 1}</th>
+              <td>
+                <div className="row">
+                  <input
+                    className="form-check-input px-2 ml-2 col-auto"
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={() => handleStatusChange(task)}
+                    id={task.id}
+                  />
+                  <label
+                    className="form-check-label mx-1 col-auto"
+                    htmlFor={task.id}
+                  >
+                    {task.completed ? "Done" : "Pending"}
+                  </label>
+                </div>
+              </td>
+              <td>{task.title}</td>
+              <td>{task.description}</td>
+              <td>{task.dueDate.toDateString()}</td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleDelete(task)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TaskTable;
